a14c735d9033fb1ad4b3bac627a759e1ca13a77f
codesable01 <discover.developmentboot@gmail.com>
2025-01-19 11:16:33 -0500

    Descripción de los cambios en DemoApplication.java

--- src/main/java/forohub/app/api/DemoApplication.java
+++ src/main/java/forohub/app/api/DemoApplication.java
@@ -3,7 +3,7 @@ package forohub.app.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "forohub.app.api")
+@SpringBootApplication
 
 
 
